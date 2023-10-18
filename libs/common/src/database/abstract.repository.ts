146cabb5a53f43bbe82a5dc740e1e8f561e54d65
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-10-18 14:02:09 -0300

    incial setup of the project lib and its database

---
+++ libs/common/src/database/abstract.repository.ts
@@ -0,0 +1,65 @@
+import { FilterQuery, Model, Types, UpdateQuery } from 'mongoose';
+import { AbstractDocument } from './abstract.schema';
+import { Logger, NotFoundException } from '@nestjs/common';
+
+export abstract class AbstractRepository<TDocument extends AbstractDocument> {
+  protected abstract readonly logger: Logger;
+
+  constructor(protected readonly model: Model<TDocument>) {}
+
+  async create(document: Omit<TDocument, '_id'>): Promise<TDocument> {
+    const createdDocument = new this.model({
+      ...document,
+      _id: new Types.ObjectId(),
+    });
+
+    return (await createdDocument.save()).toJSON() as unknown as TDocument;
+  }
+
+  async findOne(filterQuery: FilterQuery<TDocument>): Promise<TDocument> {
+    const document = await this.model.findOne(filterQuery, {}, { lean: true });
+
+    if (!document) {
+      this.logger.warn('Document not found with filter query');
+      throw new NotFoundException('Document not found');
+    }
+    return document as TDocument;
+  }
+
+  async findOneAndUpdate(
+    filterQuery: FilterQuery<TDocument>,
+    updateQuery: UpdateQuery<TDocument>,
+  ): Promise<TDocument> {
+    const document = await this.model.findOneAndUpdate(
+      filterQuery,
+      updateQuery,
+      { lean: true, new: true },
+    );
+
+    if (!document) {
+      this.logger.warn('Document not found');
+      throw new NotFoundException('Document not found');
+    }
+
+    return document as TDocument;
+  }
+
+  async find(filterQuery: FilterQuery<TDocument>): Promise<TDocument> {
+    const document = await this.model.find(filterQuery, {}, { lean: true });
+    return document as unknown as TDocument;
+  }
+
+  async findOneAndDelete(
+    filterQuery: FilterQuery<TDocument>,
+  ): Promise<TDocument> {
+    const document = await this.model.findOneAndDelete(filterQuery, {
+      lean: true,
+    });
+
+    if (!document) {
+      this.logger.warn('Document not found');
+      throw new NotFoundException('Document not found');
+    }
+    return document as TDocument;
+  }
+}
